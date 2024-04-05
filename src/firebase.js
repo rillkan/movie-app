638efcc6f8185954088b4ad2e1b5855af1e2d904
Reagan Kan <mjkan96@gmail.com>
2024-04-05 10:12:00 +0800

    add firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8_8SMNwH_v4oZum8M0zEO964Yy8b6AUQ",
-  authDomain: "movie-app-9afe5.firebaseapp.com",
-  projectId: "movie-app-9afe5",
-  storageBucket: "movie-app-9afe5.appspot.com",
-  messagingSenderId: "606965063111",
-  appId: "1:606965063111:web:7fc842454074190ba08de2"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
