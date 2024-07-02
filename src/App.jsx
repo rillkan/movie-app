12e9c7e60608c4ae8ae41f8c24cd962c8f7180d2
Reagan Kan <mjkan96@gmail.com>
2024-07-02 21:43:07 +0800

    Added notes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,16 @@ import Diary from "./pages/Diary"
 import SearchMovies from "./pages/SearchMovies";
 
 export default function App() {
-
+  /* Deploy it to live, 
+  replace =  https://2371db49-9e80-407f-9b44-2e5dedea1a5c-00-1e1u5gz9b7dss.picard.replit.dev
+  to 
+  new = https://movie-app-backend-d3ba.onrender.com
+  1)ActualNameProvider.jsx
+  2)movieSlice.js
+  3)AuthHome.jsx
+  4)MovieDetails.jsx
+  Make sure this is running = https://replit.com/@sigma-school-full-time-capst/Capstone-Project-API-mjkan96
+  */
   return (
     <Provider store={store}>
       <AuthProvider>
